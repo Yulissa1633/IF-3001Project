a4810a872c82b641fec6429dbe79d66efa9fd68a
Yulissa1633 <yulissa@1633gmail.com>
2022-02-09 00:04:47 -0600

    Login / Register Window

--- src/GUI/StartWindow.java
+++ src/GUI/StartWindow.java
@@ -1,10 +1,13 @@
 package GUI;
 
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Label;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
@@ -16,6 +19,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
@@ -26,8 +30,18 @@ public class StartWindow extends JFrame{
 	private StartPanel registerArea;
 	private StartPanel selectCharacterArea;
 	private BufferedImage logo;
+	private BufferedImage title;
 	private BufferedImage loginButtonSkin;
+	private BufferedImage registerButtonSkin;
+	private BufferedImage texfieldBackSkin;
 	private JLabel labelLogo;
+	private JLabel labelTitle;
+	private JLabel texfieldUserBack;
+	private JLabel texfieldPassBack;
+	private JLabel labelUser;
+	private JLabel labelPass;
+	private JTextField texfieldUser;
+	private JPasswordField texfieldPass;
 	private JButton loginButton;
 	private JButton registerButton;
 
@@ -39,6 +53,9 @@ public class StartWindow extends JFrame{
 		try {
 			logo =ImageIO.read(getClass().getResourceAsStream("/Assets/logo.png"));
 			loginButtonSkin =ImageIO.read(getClass().getResourceAsStream("/Assets/loginButton.png"));
+			registerButtonSkin =ImageIO.read(getClass().getResourceAsStream("/Assets/registerButton.png"));
+			title =ImageIO.read(getClass().getResourceAsStream("/Assets/title.png"));
+			texfieldBackSkin =ImageIO.read(getClass().getResourceAsStream("/Assets/textfieldBack.png"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -48,6 +65,8 @@ public class StartWindow extends JFrame{
 	}
 	
 	private void init() {
+		
+		//-------------Sección de Bienvenida-----------------
 		this.welcomeArea = new StartPanel();
 		this.welcomeArea.setLayout(null);
 		
@@ -55,9 +74,76 @@ public class StartWindow extends JFrame{
 		this.welcomeArea.add(labelLogo);
 		labelLogo.setBounds(360,20,200,200);
 		
+		this.labelTitle = new JLabel(new ImageIcon(title));
+		this.welcomeArea.add(labelTitle);
+		labelTitle.setBounds(140,240,291,64);
+		
 		this.loginButton = new JButton(new ImageIcon(loginButtonSkin));
 		this.welcomeArea.add(loginButton);
-		loginButton.setBounds(200,350,150,33);
+		loginButton.setBounds(204,350,150,33);
+		loginButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	welcomeArea.setVisible(false);
+            	loginArea.setVisible(true);
+            }
+          });
+		
+		this.registerButton = new JButton(new ImageIcon(registerButtonSkin));
+		this.welcomeArea.add(registerButton);
+		registerButton.setBounds(204,420,150,33);
+		registerButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	welcomeArea.setVisible(false);
+            	registerArea.setVisible(true);
+            }
+          });
+		
+		//-------------Sección de Login-----------------
+		this.loginArea = new StartPanel();
+		this.loginArea.setLayout(null);
+		
+		this.labelLogo = new JLabel(new ImageIcon(logo));
+		this.loginArea.add(labelLogo);
+		labelLogo.setBounds(360,20,200,200);
+		
+		this.texfieldUserBack = new JLabel(new ImageIcon(texfieldBackSkin));
+		this.loginArea.add(texfieldUserBack, 1, 0);
+		texfieldUserBack.setBounds(170,310,227,50);
+		
+		this.labelUser = new JLabel("Usuario:");
+		this.labelUser.setFont(new Font("Perpetua", Font.PLAIN, 18));
+		this.loginArea.add(labelUser, 2, 0);
+		labelUser.setBounds(180,310,100,50);
+		
+		this.texfieldUser = new JTextField();
+		this.texfieldUser.setOpaque(false);
+		this.texfieldUser.setBorder(null);
+		this.loginArea.add(texfieldUser, 2, 0);
+		texfieldUser.setBounds(250,322,120,25);
+		
+		this.texfieldPassBack = new JLabel(new ImageIcon(texfieldBackSkin));
+		this.loginArea.add(texfieldPassBack, 1, 0);
+		texfieldPassBack.setBounds(170,380,227,50);
+		
+		
+		this.labelPass = new JLabel("Contraseña:");
+		this.labelPass.setFont(new Font("Perpetua", Font.PLAIN, 18));
+		this.loginArea.add(labelPass, 2, 0);
+		labelPass.setBounds(180,380,100,50);
+		
+		this.texfieldPass = new JPasswordField();
+		this.texfieldPass.setOpaque(false);
+		this.texfieldPass.setBorder(null);
+		this.loginArea.add(texfieldPass, 2, 0);
+		texfieldPass.setBounds(270,392,100,25);
+		
+		this.loginButton = new JButton(new ImageIcon(loginButtonSkin));
+		this.loginArea.add(loginButton);
+		loginButton.setBounds(209,450,150,33);
 		loginButton.addActionListener(new ActionListener() {
 
             @Override
@@ -66,9 +152,48 @@ public class StartWindow extends JFrame{
             }
           });
 		
-		this.registerButton = new JButton("Registrarse");
-		this.welcomeArea.add(registerButton);
-		registerButton.setBounds(200,420,120,40);
+		//-------------Sección de Registro-----------------
+		this.registerArea = new StartPanel();
+		this.registerArea.setLayout(null);
+		
+		this.labelLogo = new JLabel(new ImageIcon(logo));
+		this.registerArea.add(labelLogo);
+		labelLogo.setBounds(360,20,200,200);
+		
+		this.texfieldUserBack = new JLabel(new ImageIcon(texfieldBackSkin));
+		this.registerArea.add(texfieldUserBack, 1, 0);
+		texfieldUserBack.setBounds(170,310,227,50);
+		
+		this.labelUser = new JLabel("Usuario:");
+		this.labelUser.setFont(new Font("Perpetua", Font.PLAIN, 18));
+		this.registerArea.add(labelUser, 2, 0);
+		labelUser.setBounds(180,310,100,50);
+		
+		this.texfieldUser = new JTextField();
+		this.texfieldUser.setOpaque(false);
+		this.texfieldUser.setBorder(null);
+		this.registerArea.add(texfieldUser, 2, 0);
+		texfieldUser.setBounds(250,322,120,25);
+		
+		this.texfieldPassBack = new JLabel(new ImageIcon(texfieldBackSkin));
+		this.registerArea.add(texfieldPassBack, 1, 0);
+		texfieldPassBack.setBounds(170,380,227,50);
+		
+		
+		this.labelPass = new JLabel("Contraseña:");
+		this.labelPass.setFont(new Font("Perpetua", Font.PLAIN, 18));
+		this.registerArea.add(labelPass, 2, 0);
+		labelPass.setBounds(180,380,100,50);
+		
+		this.texfieldPass = new JPasswordField();
+		this.texfieldPass.setOpaque(false);
+		this.texfieldPass.setBorder(null);
+		this.registerArea.add(texfieldPass, 2, 0);
+		texfieldPass.setBounds(270,392,100,25);
+		
+		this.registerButton = new JButton(new ImageIcon(registerButtonSkin));
+		this.registerArea.add(registerButton);
+		registerButton.setBounds(209,450,150,33);
 		registerButton.addActionListener(new ActionListener() {
 
             @Override
@@ -77,9 +202,11 @@ public class StartWindow extends JFrame{
             }
           });
 		
-		
-		
 		this.add(welcomeArea);
+		this.add(loginArea);
+		this.add(registerArea);
+		this.loginArea.setVisible(false);
+		this.registerArea.setVisible(false);
 	}
 	
 }
